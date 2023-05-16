f90fa0d1b72adbb213ed534815b8e27002cfdd02
Trần Đức Nam <dynamo.foss@gmail.com>
2023-05-16 23:47:50 +0700

    refactor: use scripts to install plugins when use chezmoi

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 DIR=$(dirname "$(readlink -f "$0")")
 
 git -C "$SETUP_DIR" submodule update --init --recursive --remote
-bash "$DIR/plugins/tpm/bin/install_plugins"
+bash "$DIR/plugins/tpm/scripts/install_plugins.sh"
